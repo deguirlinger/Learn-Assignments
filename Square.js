1b14fe2f01de45759f2d3675ffbdc72b3b80cc83
deguirlinger <43054336+deguirlinger@users.noreply.github.com>
2018-12-18 14:50:45 -0800

    Add files via upload

---
+++ Square.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Square extends Component {
+
+  render() {
+      return (
+        <div className="Square">
+        ?
+        </div>
+    );
+  }
+}
+
+
+
+export default Square;
